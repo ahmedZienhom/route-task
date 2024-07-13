926b3b9d0afb1d60125d15c8ef051f2691754187
unknown <ahmedalnafy@gmail.com>
2024-07-13 03:31:02 +0300

    better search

--- js/index.js
+++ js/index.js
@@ -16,25 +16,25 @@ function getTotal(id) {
     return [total,numbers];
 }
 
-function view() {
+function view(objectArr) {
     let container = ``;
-    for(let i = 0 ; i < details.customers.length; i++) {
+    for(let i = 0 ; i < objectArr.length; i++) {
         container += ` <div class="accordion-item">
             <h2 class="accordion-header">
               <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse${collapseNum[i]}" aria-expanded="true" aria-controls="collapseOne">
-              ${details.customers[i].name}
+              ${objectArr[i].name}
               </button>
             </h2>
             <div id="collapse${collapseNum[i]}" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
               <div class="accordion-body row">
                     <div class="col-md-8 ps-3 pt-3">
-                        <p class="text-capitalize fs-5 mt-2">id: <span class="font-bold">${details.customers[i].id}</span></p>
-                        <p class="text-capitalize fs-5 mt-2">name: <span class="font-bold">${details.customers[i].name}</span></p>
-                        <p class="text-capitalize fs-5 mt-2">total of transactions: <span class="font-bold">${getTotal(details.customers[i].id)[0]}</span></p>
-                        <p class="text-capitalize fs-5 mt-2">numbers of transactions: <span class="font-bold">${getTotal(details.customers[i].id)[1]}</span></p>
+                        <p class="text-capitalize fs-5 mt-2">id: <span class="font-bold">${objectArr[i].id}</span></p>
+                        <p class="text-capitalize fs-5 mt-2">name: <span class="font-bold">${objectArr[i].name}</span></p>
+                        <p class="text-capitalize fs-5 mt-2">total of transactions: <span class="font-bold">${getTotal(objectArr[i].id)[0]}</span></p>
+                        <p class="text-capitalize fs-5 mt-2">numbers of transactions: <span class="font-bold">${getTotal(objectArr[i].id)[1]}</span></p>
                     </div>
                     <div class="col-md-4 d-flex align-items-center justify-content-center">
-                        <button data-id="${details.customers[i].id}" class="view btn btn-outline-primary">
+                        <button data-id="${objectArr[i].id}" class="view btn btn-outline-primary">
                             <i class="fa-solid mb-4 fa-chart-simple fa-xl block"></i>
                             <p>view transactions graph</p>
                         </button>
@@ -55,23 +55,33 @@ async function data() {
 
 
     details = data;
-    view();
+    view(data.customers);
+
+
+
+
     buttons = document.querySelectorAll(`button.view`);
     for(let i = 0; i < buttons.length;i++) {
         buttons[i].addEventListener(`click`, function() {
-
             let dataArr = filterData(buttons[i].getAttribute(`data-id`));
             chart(dataArr);
             document.querySelector(`div.graph`).classList.replace(`d-none`, `d-flex`);
         })
     }
+
+
+    
     search.addEventListener(`click`, function() {
       const inputValue = document.querySelector(`input[type="search"]`).value;
       document.querySelector(`input[type="search"]`).value = '';
 
+      console.log(inputValue.length);
+
       if(isNaN(inputValue)) {
         for(let i = 0 ; i < details.customers.length;i++) {
-          if((details.customers[i].name).toLowerCase() == inputValue.toLowerCase()) {
+
+          if(details.customers[i].name.toLowerCase().includes(inputValue.toLowerCase())) {
+        
             let searchFilter = filterData(details.customers[i].id);
             chart(searchFilter);
             document.querySelector(`div.graph`).classList.replace(`d-none`, `d-flex`);
@@ -90,20 +100,24 @@ async function data() {
       }}
       document.querySelector(`.alert`).classList.remove(`d-none`);
 })
+
 }
 
 function filterData(id) {
     let amounts = [];
     let dates = [];
+    let addCustomerDetails = ``;
     let addTransaction = `            <div class="col-6">
                 <p class="text-center fw-bold">date</p>
             </div>
             <div class="col-6">
                 <p class="text-center fw-bold">amount</p>
             </div>`;
+    
     for(let i = 0; i < details.transactions.length; i++) {
         if(details.transactions[i].customer_id == id) {
-            addTransaction += ` <div class="col-6">
+            addTransaction += ` 
+            <div class="col-6">
                 <p class="text-center fw-bold">${details.transactions[i].date}</p>
             </div>
              <div class="col-6">
@@ -116,11 +130,18 @@ function filterData(id) {
 }
 for (let i = 0; i < details.customers.length;i++) {
   if(details.customers[i].id == id) {
+    addCustomerDetails = `<div class=" ps-3 pt-3">
+    <p class="text-capitalize fs-5 mt-2">id: <span class="font-bold">${details.customers[i].id}</span></p>
+    <p class="text-capitalize fs-5 mt-2">name: <span class="font-bold">${details.customers[i].name}</span></p>
+    <p class="text-capitalize fs-5 mt-2">total of transactions: <span class="font-bold">${getTotal(details.customers[i].id)[0]}</span></p>
+    <p class="text-capitalize fs-5 mt-2">numbers of transactions: <span class="font-bold">${getTotal(details.customers[i].id)[1]}</span></p>
+</div>`;
     document.title = `${details.customers[i].name} graph`;
   }
 }
 document.documentElement.scrollTop = 0;
 document.body.classList.add(`overflow-y-hidden`);
+document.querySelector(`.detailsContainer`).innerHTML = addCustomerDetails;
 document.querySelector(`.history`).innerHTML = addTransaction;
 return [amounts, dates];
 };
@@ -137,9 +158,9 @@ function chart(doubleArr) {
     data: {
       labels: [...doubleArr[1]],
       datasets: [{
-        label: '# of Votes',
+        label: 'amount of transition',
         data: [...doubleArr[0]],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
@@ -169,3 +190,4 @@ document.querySelector(`input[type="search"]`).addEventListener(`focus`, functio
   document.querySelector(`.alert`).classList?.add(`d-none`);
 })
 document.querySelector(`div.bg-opacity-100`).addEventListener(`click`, e => e.stopPropagation());
+
